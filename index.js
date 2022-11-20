1a012ac9a53a95ad0288ceb2d6910aa090e98c98
markizapoimeniliza <98239628+markizapoimeniliza@users.noreply.github.com>
2022-11-19 17:26:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@
         }
         else if (!off && seconds > 0){
             --seconds
-            timer.innerHTML = `<span style= "position: relative; top: -1vh; font-size: 2.7vmin; left: -1vw" >${seconds}seconds</span>`
+            timer.innerHTML = `<span style= "position: relative; top: -1vh; font-size: 2.7vmin; left: -3vw" >${seconds}seconds</span>`
         }
         else{}
     }, 1000)
